89e138812e3251c9dcef82ccd939db67d91b582f
aevartg <aevartg@gmail.com>
2017-03-11 22:19:57 +0000

    topTep no duplicates when button pressed twice

--- Client/src/app/details-page/details-page.component.ts
+++ Client/src/app/details-page/details-page.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, ActivatedRoute} from "@angular/router";
-import { Salesperson } from '../salesperson';
-import { Product } from '../product';
-import { InfoServiceService } from '../info-service.service';
-import { FormBuilder, Validators } from '@angular/forms';
+import {Salesperson} from '../salesperson';
+import {Product} from '../product';
+import {InfoServiceService} from '../info-service.service';
+import {FormBuilder, Validators} from '@angular/forms';
 
 
 @Component({
@@ -13,27 +13,27 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class DetailsPageComponent implements OnInit {
 
-  sellerId : number;
-  seller : Salesperson;
-  allProducts : Product[];
-  topTenProducts : Product[];
+  sellerId: number;
+  seller: Salesperson;
+  allProducts: Product[];
+  topTenProducts: Product[];
 
-  newProduct : Product;
+  newProduct: Product;
 
   private addProductForm = this.fb.group({
-        id : [0],
-        quantitySold : [0],
-        quantityInStock : [0],
-        imagePath : [""],
-        name : [""],
-        price : []
+    id: [0],
+    quantitySold: [0],
+    quantityInStock: [0],
+    imagePath: [""],
+    name: [""],
+    price: []
   });
 
 
-  constructor(private fb : FormBuilder,
-              private router : Router,
-              private route : ActivatedRoute,
-              private infoService : InfoServiceService) {
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private route: ActivatedRoute,
+              private infoService: InfoServiceService) {
     this.topTenProducts = [];
   }
 
@@ -41,39 +41,39 @@ export class DetailsPageComponent implements OnInit {
     this.sellerId = this.route.snapshot.params['id'];
 
     this.infoService.getSellerbyID(this.sellerId).subscribe(result => {
-        this.seller = result;
-        console.log(this.seller);
+      this.seller = result;
+      console.log(this.seller);
     });
 
     this.infoService.getSellerProducts(this.sellerId).subscribe(result => {
-        this.allProducts = result;
+      this.allProducts = result;
     });
   }
 
-  getTopTen(){
-      for(const i in this.allProducts){
-          this.topTenProducts.push(this.allProducts[i]);
-          this.topTenProducts.sort(this.compare);
-          this.topTenProducts = this.topTenProducts.slice(0,10);
-      }
+  getTopTen() {
+    this.topTenProducts = [];
+    for (const i in this.allProducts) {
+      this.topTenProducts.push(this.allProducts[i]);
+      this.topTenProducts.sort(this.compare);
+      this.topTenProducts = this.topTenProducts.slice(0, 10);
+    }
   }
 
-  compare(a : Product ,b : Product){
-      if(a.quantitySold < b.quantitySold ){
-          return -1;
-      }
-      if(a.quantitySold > b.quantitySold){
-          return 1;
-      }
-      return 0;
+  compare(a: Product, b: Product) {
+    if (a.quantitySold < b.quantitySold) {
+      return -1;
+    }
+    if (a.quantitySold > b.quantitySold) {
+      return 1;
+    }
+    return 0;
 
   }
 
-  onNewProduct(event){
+  onNewProduct(event) {
     this.newProduct = this.addProductForm.value;
-    this.infoService.addProduct(this.newProduct, this.sellerId).subscribe(result =>
-    {
-        this.allProducts.push(result);
+    this.infoService.addProduct(this.newProduct, this.sellerId).subscribe(result => {
+      this.allProducts.push(result);
     });
     this.getTopTen();
   }
