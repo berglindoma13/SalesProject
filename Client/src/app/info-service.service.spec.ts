9ad5cd388091fa146864c9087996f408399a3a9d
aevartg <aevartg@gmail.com>
2017-03-14 16:49:14 +0000

    service test

--- Client/src/app/info-service.service.spec.ts
+++ Client/src/app/info-service.service.spec.ts
@@ -1,7 +1,7 @@
-import {TestBed, inject, async, getTestBed} from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
 import {InfoServiceService} from './info-service.service';
-import { MockBackend, MockConnection } from '@angular/http/testing';
-import { Http, BaseRequestOptions, Response, ResponseOptions, RequestOptions } from '@angular/http';
+import {Http, BaseRequestOptions, Response, ResponseOptions, RequestMethod} from '@angular/http';
+import {MockBackend, MockConnection} from '@angular/http/testing';
 
 /**
  * it should try to issue a HTTP GET request when asked for a list of sellers (expect(mockHttp.get).toHaveBeenCalled();)
@@ -11,7 +11,13 @@ import { Http, BaseRequestOptions, Response, ResponseOptions, RequestOptions } f
 
 describe('InfoServiceService', () => {
   let service: InfoServiceService = null;
-  let backend: MockBackend;
+  let backend: MockBackend = null;
+  let seller = {
+    id: 1,
+    name: "prufa",
+    category: "fatnaður",
+    imagePath: "image"
+  };
 
 
   beforeEach(() => {
@@ -22,7 +28,7 @@ describe('InfoServiceService', () => {
         BaseRequestOptions,
         {
           provide: Http,
-          useFactory: (mockBackend: MockBackend, defaultOptions: RequestOptions) => {
+          useFactory: (mockBackend, defaultOptions) => {
             return new Http(mockBackend, defaultOptions);
           },
           deps: [MockBackend, BaseRequestOptions]
@@ -32,92 +38,90 @@ describe('InfoServiceService', () => {
   });
 
   beforeEach(inject([InfoServiceService, MockBackend], (info, mockBackend) => {
-    let subject = info;
-    let backend = mockBackend;
+     service = info;
+     backend = mockBackend;
   }));
 
 
   it('should get profile data of user', (done) => {
-    let profileInfo = { id: 1, name: 'Tester', category: 'Föt', imagePath: 'https://www.w3schools.com/css/img_fjords.jpg'};
-    backend.connections.subscribe((connection: MockConnection) => {
-      let options = new ResponseOptions({ body: profileInfo });
+    backend.connections.subscribe((c: MockConnection) => {
+      let connection = c;
+      let options = new ResponseOptions({body: seller});
 
       connection.mockRespond(new Response(options));
     });
 
     service.getSellerbyID(1).subscribe((response) => {
-      expect(response).toBe(profileInfo);
+      expect(response).toEqual(seller);
       done();
     });
-  });
 
-
-
+  })
 });
 /*
-describe('InfoServiceService', () => {
-  let service: InfoServiceService = null;
+ describe('InfoServiceService', () => {
+ let service: InfoServiceService = null;
 
-  const mockHttp = {
-    get: jasmine.createSpy('get'),
-    post: jasmine.createSpy('post'),
-    put: jasmine.createSpy('put'),
-    map: jasmine.createSpy('map')
-  };
+ const mockHttp = {
+ get: jasmine.createSpy('get'),
+ post: jasmine.createSpy('post'),
+ put: jasmine.createSpy('put'),
+ map: jasmine.createSpy('map')
+ };
 
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [{
-        provide: Http,
-        useValue: mockHttp
-      }, InfoServiceService]
-    });
-  });
+ beforeEach(() => {
+ TestBed.configureTestingModule({
+ providers: [{
+ provide: Http,
+ useValue: mockHttp
+ }, InfoServiceService]
+ });
+ });
 
-  /*it('should return 1', inject([InfoServiceService], (service: InfoServiceService) => {
-   expect(service.testingUnitTests()).toBe(1);
-   }));*/
+ it('should return 1', inject([InfoServiceService], (service: InfoServiceService) => {
+ expect(service.testingUnitTests()).toBe(1);
+ }));
 
-  /*it('should get one seller by id', async(() => {
-   let infoService: InfoServiceService = getTestBed.get(InfoServiceService);
-   }));
+ it('should get one seller by id', async(() => {
+ let infoService: InfoServiceService = getTestBed.get(InfoServiceService);
+ }));
 
-   it('should call get in getAllSellers', inject([InfoServiceService], (service: InfoServiceService) => {
-   service.getAllSellers();
-   expect(mockHttp.get).toHaveBeenCalled();
-   }));
+ it('should call get in getAllSellers', inject([InfoServiceService], (service: InfoServiceService) => {
+ service.getAllSellers();
+ expect(mockHttp.get).toHaveBeenCalled();
+ }));
 
-   it('should call get in getAllSellers', ()=>{
-   service.getAllSellers();
-   expect(mockHttp.get).toHaveBeenCalled();
-   });
+ it('should call get in getAllSellers', ()=>{
+ service.getAllSellers();
+ expect(mockHttp.get).toHaveBeenCalled();
+ });
 
-   it('should ...', inject([InfoServiceService], (service: InfoServiceService) => {
-   expect(service).toBeTruthy();
-   }));
-   });
+ it('should ...', inject([InfoServiceService], (service: InfoServiceService) => {
+ expect(service).toBeTruthy();
+ }));
+ });*/
+/*
+ describe('HomeService', () => {
+ let service: InfoServiceService, mockHttp;
 
-  describe('HomeService', () => {
-    let service: InfoServiceService, mockHttp;
+ beforeEach(() => {
+ const mockHttp = {
+ get: jasmine.createSpy('get'),
+ post: jasmine.createSpy('post'),
+ put: jasmine.createSpy('put'),
+ map: jasmine.createSpy('map')
+ };
 
-    beforeEach(() => {
-      const mockHttp = {
-        get: jasmine.createSpy('get'),
-        post: jasmine.createSpy('post'),
-        put: jasmine.createSpy('put'),
-        map: jasmine.createSpy('map')
-      };
+ });
+ describe('GetbyID', () => {
+ it('should get the detail of user with passed id', () => {
+ let user = {id: 1, name: 'Kalli'};
+ mockHttp.get.and.returnValue(Observable.of(false));
+ service.getSellerbyID(1);
+ expect(mockHttp.get).toHaveBeenCalledWith('http://localhost:5000/api/sellers/1');
 
-    });
-    describe('GetbyID', () => {
-      it('should get the detail of user with passed id', () => {
-        let user = {id: 1, name: 'Kalli'};
-        mockHttp.get.and.returnValue(Observable.of(false));
-        service.getSellerbyID(1);
-        expect(mockHttp.get).toHaveBeenCalledWith('http://localhost:5000/api/sellers/1');
-
-      });
-    });
-  });
+ });
+ });
+ });*/
 
